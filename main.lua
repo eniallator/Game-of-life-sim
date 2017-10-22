9fb5edb606a88badf81e78b1b8ff06f269849e76
eniallator <eniallatormc@gmail.com>
2017-10-22 15:05:38 +0100

    Fixed bugs in rules and have simulation working

--- main.lua
+++ main.lua
@@ -1,15 +1,25 @@
-local screenDim, aliveCells, zoom, golRules, display = {}, {}, 0.4
+local paused, applyRules, display
+local screenDim = {}
+local aliveCells = {{20, 14}, {19, 15}, {20, 15}, {20, 16}, {21, 16}}
+local zoom = 2
+local speed = 1
+local timer = 0
 
 function love.load(args)
     love.graphics.setDefaultFilter("nearest", "nearest")
     screenDim.x, screenDim.y = love.graphics.getDimensions()
 
-    golRules = require 'src.GolRules'
+    applyRules = require 'src.GolRules'
     display = require 'src.Display'
 end
 
 function love.update(dt)
+    while timer > 1 / speed do
+        timer = timer - 1 / speed
+        aliveCells = applyRules(aliveCells)
+    end
 
+    timer = timer + dt
 end
 
 function love.draw()
--- src/GolRules.lua
+++ src/GolRules.lua
@@ -6,8 +6,8 @@ local function findDeadNeighbours(cycle, index)
         for y=-1, 1 do
             if x ~= 0 or y ~= 0 then
                 local nextCell = {
-                    cycle[index + x],
-                    cycle[index + y]
+                    cycle[index][1] + x,
+                    cycle[index][2] + y
                 }
 
                 if not utils.find(nextCell, cycle) then
@@ -21,7 +21,7 @@ local function findDeadNeighbours(cycle, index)
 end
 
 local function stackNeighbours(total, neighbours)
-    for cell in neighbours do
+    for _, cell in pairs(neighbours) do
         local added = false
 
         for _, pos in pairs(total) do
@@ -38,18 +38,30 @@ local function stackNeighbours(total, neighbours)
     end
 end
 
+local function spawnNewCells(nextCycle, totalDeadNeighbours)
+    for _, pos in pairs(totalDeadNeighbours) do
+        if pos[3] == 3 then
+            table.insert(nextCycle, {pos[1], pos[2]})
+        end
+    end
+    
+    return nextCycle
+end
+
 local function applyRules(prevCycle)
     local totalDeadNeighbours = {}
     local nextCycle = utils.copy(prevCycle)
-
+    
     for i=#prevCycle, 1, -1 do
-        deadNeighbours = findDeadNeighbours(prevCycle, i)
+        local deadNeighbours = findDeadNeighbours(prevCycle, i)
         stackNeighbours(totalDeadNeighbours, deadNeighbours)
-
+        
         if #deadNeighbours > 6 or #deadNeighbours < 5 then
             table.remove(nextCycle, i)
         end
     end
+    
+    return spawnNewCells(nextCycle, totalDeadNeighbours)
 end
 
 return applyRules
